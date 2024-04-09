3aa8d0210fb5e0ee12241039f7886d060ef51cf0
WANE Merlin <wanefkdd@gmail.com>
2024-04-09 19:21:42 +0100

    core: prepare component for list books

---
+++ FRONT-END/my-ludotek-ws/projects/book-features/src/lib/components/list-books/list-books.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dtbc-list-books',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-books.component.html',
+  styleUrl: './list-books.component.css'
+})
+export class ListBooksComponent {
+
+}
--- FRONT-END/my-ludotek-ws/projects/book-features/src/public-api.ts
+++ FRONT-END/my-ludotek-ws/projects/book-features/src/public-api.ts
@@ -2,4 +2,5 @@
  * Public API Surface of book-features
  */
 
- 
+//export * from './lib/book-features.service';
+export * from './lib/components/list-books/list-books.component';

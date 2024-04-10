04418a66dd67ffce1c65e82ac7b7229f37434e86
WANE Merlin <wanefkdd@gmail.com>
2024-04-10 06:53:41 +0100

    feat: lazy loading book

---
+++ FRONT-END/my-ludotek-ws/projects/book-features/src/lib/books.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { PageListBooksComponent } from "../public-api";
+
+
+export const booksRoutes : Routes = [
+    {
+        path: "", 
+        component: PageListBooksComponent
+    }
+]
--- FRONT-END/my-ludotek-ws/projects/book-features/src/public-api.ts
+++ FRONT-END/my-ludotek-ws/projects/book-features/src/public-api.ts
@@ -3,4 +3,11 @@
  */
 
 //export * from './lib/book-features.service';
+
 export * from './lib/components/list-books/list-books.component';
+export * from './lib/pages/page-list-books/page-list-books.component';
+export * from './lib/books.routes';
+
+ 
+
+
